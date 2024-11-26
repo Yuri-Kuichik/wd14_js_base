7822591479d5f4a9d2f61abbafb9afdc7316eed6
Ramirez <roman.shevtsov.dev@gmail.com>
2024-11-26 13:40:05 +0300

    Add complete script.js for hw2 script

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,3 +1,7 @@
-userName = prompt('Введите ваше имя: ')
+let userName = prompt('Введите ваше имя: ')
 
 alert(`Рад приветствовать вас, ${userName}`)
+
+let answerToQuestion = prompt('В каком году появился ES6? ')
+
+answerToQuestion ===  '2015' ? alert("Правильно!. Вы такой умный!") : alert("Неверно! Google вам в помощь!")
