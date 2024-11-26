29f02aef4e4f757933e82ed8406f6f5a681dfa9c
Chyliuk <cilukivan@gmail.com>
2024-11-26 18:59:23 +0300

    hw1

---
+++ 01-basics/chyliuk/script.js
@@ -0,0 +1,10 @@
+let userName = prompt("Введите ваше имя")
+alert("Добро пожаловать, " + userName)
+
+let answer = prompt("ES6 появился в 2015 году? (да/нет)")
+if(answer === "да"){
+    alert("Вы такой умный")
+}
+else{
+    alert("Неверно! Google вам в помощь!")
+}
