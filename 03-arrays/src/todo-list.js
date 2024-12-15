e12223b22148010f2d9e7430f84f85f332ecfde6
Sheezq <matiuchov@gmail.com>
2024-12-15 19:15:12 +0200

    todolist

--- 03-arrays/src/todo-list.js
+++ 03-arrays/src/todo-list.js
@@ -1,30 +1,56 @@
-const todoListData = [
-    {
-      text: 'Do homework!',
-      completed: true,
-    },
-    {
-      text: 'Do exercise!',
-      completed: true,
-    },
-    {
-      text: 'Read JS book!',
-      completed: false,
-    }
-  ]
-  
-  // находим шаблон в html (template)
-  
-  // находим контейнер в который будем добавлять шаблоны
-  
-  // Выполняем итерацию по массиву todoListData. По каждой итерации выполняем:
-  
-    // Клонируем содержимое тега <template> в переменную
-    // const todoItem = 
-  
-    // Находим чекбокс и ставим активен ли он в зависимости от статуса задачи (completed)
-    // todoItem.querySelector()
-    // Находим нужный тег и помещаем текст внутрь
-    // todoItem.querySelector()
-  
-    // Вставляем склонированный контент в контейнер
+const todoListData = JSON.parse(localStorage.getItem('todoListData')) || [
+  { text: 'Do homework!', completed: true },
+  { text: 'Do exercise!', completed: true },
+  { text: 'Read JS book!', completed: false }
+];
+
+const template = document.getElementById("todo-item");
+const container = document.getElementById("todo-container");
+const addTaskButton = document.getElementById("addTaskButton");  
+const modal = document.getElementById("modal");
+const modalCancelButton = document.getElementById("modal-cancel");
+const modalAddButton = document.getElementById("modal-add");
+const modalInput = document.getElementById("taskInput");  
+
+let editIndex = -1;
+
+function renderTodoList() {
+  container.innerHTML = '';
+  todoListData.forEach((task) => {
+    const todoItem = template.content.cloneNode(true);
+
+    const checkbox = todoItem.querySelector(".todo-list__checkbox");
+    checkbox.checked = task.completed;
+
+    const textElement = todoItem.querySelector(".todo-list__text");
+    textElement.textContent = task.text;
+
+    container.appendChild(todoItem);
+  });
+}
+
+renderTodoList();
+
+addTaskButton.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+});
+
+modalCancelButton.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  modalInput.value = '';
+});
+
+modalAddButton.addEventListener('click', () => {
+  const newTaskText = modalInput.value.trim();
+
+  if (newTaskText !== '') {
+    const newTask = { text: newTaskText, completed: false };
+    todoListData.push(newTask);
+    localStorage.setItem('todoListData', JSON.stringify(todoListData));
+    renderTodoList();
+    modal.classList.add('hidden');
+    modalInput.value = '';
+  } else {
+    alert('Please enter a task');
+  }
+});
