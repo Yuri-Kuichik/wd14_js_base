d83023e333a37d4764d0cf364e218d4fea6f3dd2
Yuri Kuichik <kuichikmis4enko@gmail.com>
2025-01-14 22:36:38 +0300

    vue app CDN

---
+++ vue-app/app.js
@@ -0,0 +1,17 @@
+const App1 = Vue.createApp({
+    data() {
+        return {
+            count: 0
+        }
+    },
+
+    methods: {
+
+    },
+
+    computed: {
+
+    }
+})
+
+App1.mount('#app1')
